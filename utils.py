0fdea16993a05f652364f8ae2766c7b119395c9f
stephaneurbon-hue <stephane.urbon@gmail.com>
2025-10-27 19:07:19 +0100

    Delete timer management functions from utils.py
    Remove utility functions for managing Tkinter timers.

--- utils.py
+++ utils.py
@@ -1,16 +1,1 @@
-"""
-Fonctions utilitaires pour la gestion des timers Tkinter
-"""
 
-def set_timer(root, name, delay, callback, obj):
-    tid = root.after(delay, callback)
-    setattr(obj, name, tid)
-
-def cancel_timer(root, name, obj):
-    tid = getattr(obj, name, None)
-    if tid:
-        try:
-            root.after_cancel(tid)
-        except Exception:
-            pass
-        setattr(obj, name, None)
