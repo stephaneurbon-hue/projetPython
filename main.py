e9d253dcbd663d812cc39f4d40934219feccc4dc
ZinedineRayane <zidane.rayanes@gmail.com>
2025-10-27 20:11:53 +0100

    Replace MenuApp with InterfaceAccueil in main.py

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 import tkinter as tk
-<<<<<<< HEAD
-from menu import MenuApp
-
-def main():
-    root = tk.Tk()
-    MenuApp(root)
-=======
 from menu import InterfaceAccueil
 
 def main():
     root = tk.Tk()
     InterfaceAccueil(root)
->>>>>>> 0fdea16993a05f652364f8ae2766c7b119395c9f
     root.mainloop()
 
 if __name__ == "__main__":
     main()
-
