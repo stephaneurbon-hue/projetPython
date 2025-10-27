3f4da3dfa08773e7b557354a46093d373c10b7ce
ZinedineRayane <zidane.rayanes@gmail.com>
2025-10-27 19:03:22 +0100

    Refactor main.py to use main function and new interface

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-"""
-Fichier principal du jeu Casse-Brique
--------------------------------------
-Auteur : Stéphane Urbon & Rayane Zidane
-Date   : 27/10/2025
-Rôle   : Lance le menu principal.
-"""
-
 import tkinter as tk
-from menu import MenuApp  # nom du module en minuscules
+from menu import InterfaceAccueil
+
+def main():
+    root = tk.Tk()
+    InterfaceAccueil(root)
+    root.mainloop()
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = MenuApp(root)
-    root.mainloop()
+    main()
+
